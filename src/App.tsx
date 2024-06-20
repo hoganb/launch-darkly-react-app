f926a534e45c898f309c7547bb84ca51f9b32f04
Ben Hogan <sweeetata@gmail.com>
2024-06-20 12:13:23 +0100

    refactor: remove test1 feature flag

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import { useFlags } from 'launchdarkly-react-client-sdk';
 
 function App() {
-  const { test1 } = useFlags();
-
   return (
       <div className="App">
-        <header className="App-header" style={{backgroundColor: test1 ? '#00844B' : '#373841'}}>
-            <p>The test1 feature flag evaluates to <b>{test1 ? 'True' : 'False'}</b></p>
+        <header className="App-header" style={{backgroundColor: '#00844B'}}>
+            <p>Feature flag removed</p>
         </header>
       </div>
   );
