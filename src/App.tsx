f844bc007959d4b17dfd02e2820b0835f0de904e
Ben Hogan <sweeetata@gmail.com>
2024-06-20 12:35:15 +0100

    fix: add test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import { useFlags } from 'launchdarkly-react-client-sdk';
 
 function App() {
+  const { test1, test2 } = useFlags();
+
   return (
       <div className="App">
-        <header className="App-header" style={{backgroundColor: '#00844B'}}>
-            <p>Feature flag removed</p>
+        <header className="App-header" style={{backgroundColor: test2 ? '#00844B' : '#373841'}}>
+            <p>The test1 feature flag evaluates to <b>{test1 ? 'True' : 'False'}</b></p>
         </header>
       </div>
   );
